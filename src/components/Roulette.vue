cbad1b54769aedd9120aabec58f54e28760726ce
adambodie <adam.bodie@yahoo.com>
2019-05-05 15:27:10 -0700

    Updated styles for Roulette, Slick

--- src/components/Roulette.vue
+++ src/components/Roulette.vue
@@ -2,6 +2,7 @@
 	<div class="roulette">
 	<h1>Roulette</h1>
 	<div id="wheel" v-bind:class="{ activeRoulette: isActive }" v-bind:style="'--rotation: ' + rotation + ';'"></div>
+	<div id="ball" v-bind:class="{ activeBall: isActive }"></div>
 	<div class="buttons">
 	<button class="owl-next" @click="spin()">Spin</button>
 	<button class="owl-next" @click="reset()">Reset</button>
@@ -42,11 +43,23 @@ export default {
 	align-items: center;
 	margin: 0 auto;
 	width: 60%;
+	position: relative;
 }
 .roulette h1 {
 	font-family: 'Monoton', cursive;
 	color: #F14285;
 }
+
+#ball {
+	width: 25px;
+	height: 25px;
+	border-radius: 100%;
+	background-image: radial-gradient(circle, green, black);
+	position: absolute;
+	opacity: 0;
+	top: 50%;
+}
+
 #wheel {
   width: 400px;
   height: 400px;
@@ -59,9 +72,17 @@ export default {
   animation: spinning 2.0s forwards;
 }
 
+.activeBall {
+  animation: ball 2.0s ease-out 0.25s forwards;
+}
 @keyframes spinning {
   0% { transform: rotate(0); }
   100% { transform: rotate(var(--rotation)); }
+}
+
+@keyframes ball {
+  0% { opacity: 0; top: 50%;}
+  100% { opacity: 1; top: 25%; }
 }
 
 </style>
